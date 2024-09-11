a7b4f07073f7c203001cb8676ddcf6f3d3f62d7d
Aditya Vr <adityaverma4768@gmail.com>
2024-09-12 03:15:24 +0530

    C++

--- Cpp_04.cpp
+++ Cpp_04.cpp
@@ -36,10 +36,11 @@ int main(){
 
 
 
+
 //__________________String Array______________
-#include<bits/stdc++.h>
+/*#include<bits/stdc++.h>
 using namespace std;
 int main(){
     string name = "Aditya";
     cout<< name[0] << " " << name[2] << " " << name[5];
-}
+}*/
---
+++ Cpp_05.cpp
@@ -0,0 +1,61 @@
+//_____________While Loop__________
+/*#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int num = 5;
+    while(num!=0){
+        cout<<num<<endl;
+        num--;
+    }
+}*/
+
+
+
+
+//____________For Loop_____________
+/*#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int num1 = 5;
+    for(int i=1; i<=num1; i++){
+        cout<< i << endl;
+    }
+}*/
+
+
+
+
+
+
+//__________Do While_______________
+/*#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int num = 5;
+    do{
+        cout << num;
+        num--;
+    }while(num==0);
+}*/
+
+
+
+
+
+//__________Array With For Loop__________
+/*#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int ArrSize;
+    cout<<"Enter the size of Array: ";
+    cin>>ArrSize;
+    int arr[ArrSize];
+    cout<<"Enter the Array Value: "<<endl;
+    for(int i=0; i<ArrSize; i++){
+        cin >> arr[i];
+    }
+    cout<< "Values are: "<< endl;
+    for(int i=0; i<ArrSize; i++){
+        cout << arr[i] << endl;
+    }
+}*/
