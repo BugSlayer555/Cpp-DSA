6de1eeb89c828f928671f37732a091aeabdd1b55
Aditya Vr <adityaverma4768@gmail.com>
2024-09-18 21:08:40 +0530

    C++

---
+++ Cpp_06.cpp
@@ -0,0 +1,67 @@
+//__________Void Function Without Parameter____________
+/*#include<bits/stdc++.h>
+using namespace std;
+void PrintName(){
+    cout<<"Hey! My Name is Adi";
+}
+int main(){
+    PrintName();
+    return 0;
+}*/
+
+
+
+
+
+//___________Void Function with Parameter___________
+/*#include<bits/stdc++.h>
+using namespace std;
+void PrintName(string name){
+    cout<<"Hey! My Name is "<<name;
+}
+int main(){
+    string name1;
+    cout<<"Enter your Name: ";
+    cin>>name1;
+    PrintName(name1);
+    return 0;
+}*/
+
+
+
+
+
+
+//___________Return Function(Int) Without Parameter_________
+/*#include<bits/stdc++.h>
+using namespace std;
+int Addition(){
+    int num1 = 5, num2 = 10;
+    return num1+num2;
+} 
+int main(){
+    int res =Addition();
+    cout<<res;
+    return 0;
+}*/
+
+
+
+
+
+
+//________Return Function(Int) With Parameter__________
+#include<bits/stdc++.h>
+using namespace std;
+int Sum(int num1, int num2){
+    return num1+num2;
+}
+int main(){
+    int num1,num2;
+    cout<<"Enter First Number: ";
+    cin>> num1;
+    cout<<"Enter Second Number: ";
+    cin>>num2;
+    int result = Sum(num1,num2);
+    cout<<"Total Sum of Both Number is: "<<result;
+}
